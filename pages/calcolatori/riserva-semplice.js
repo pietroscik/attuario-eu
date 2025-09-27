03bc505389b8ba8571f56b37a0cd5f05a6e7d70e
Pietro Maietta <155844333+pietroscik@users.noreply.github.com>
2025-09-27 10:14:28 +0200

    Add files via upload

--- pages/calcolatori/riserva-semplice.js
+++ pages/calcolatori/riserva-semplice.js
@@ -1,11 +1,2 @@
-// pages/calcolatori/riserva-semplice.js
 import RiservaSemplice from "../../components/calculators/RiservaSemplice";
-
-export default function RiservaSemplicePage(){
-  return (
-    <main style={{maxWidth:900, margin:"2rem auto", padding:"1rem"}}>
-      <h1>Riserva Semplice (wrapper)</h1>
-      <RiservaSemplice />
-    </main>
-  );
-}
+export default function Page(){ return <RiservaSemplice />; }
