97bbab96c448dab8e35de847f9bb54ca08ca9a25
Pietro Maietta <155844333+pietroscik@users.noreply.github.com>
2025-09-28 21:17:30 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,48 @@
+// pages/_app.js
 import "../styles/globals.css";
-export default function MyApp({ Component, pageProps }){
-  return <Component {...pageProps} />;
+import Script from "next/script";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+const GA_MEASUREMENT_ID = 'G-HMHRTDB0B2';
+
+export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag?.('config', GA_MEASUREMENT_ID, {
+        page_path: url,
+      });
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
