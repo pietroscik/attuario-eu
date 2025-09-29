3932999b6205977e8843b3c26ed32aa38b787685
Pietro Maietta <155844333+pietroscik@users.noreply.github.com>
2025-09-29 17:09:22 +0200

    Add wiki link to navigation

--- components/Nav.js
+++ components/Nav.js
@@ -5,6 +5,7 @@ const links = [
   { href: "/applicazioni", label: "Applicazioni" },
   { href: "/attuario", label: "Attuari" },
   { href: "/risorse", label: "Risorse" },
+  { href: "/wiki", label: "Wiki" },
   { href: "/notizie", label: "Notizie" },
   { href: "/strumenti", label: "Strumenti" },
   { href: "/blog", label: "Blog" },
