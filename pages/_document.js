7a3662121f1e8c8d453629a108e73e06c4291e6c
Pietro Maietta <155844333+pietroscik@users.noreply.github.com>
2025-09-29 13:43:07 +0200

    Add AMP-aware AdSense handling

--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,49 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="it">
-      <Head>
-        <meta
-          name="google-adsense-account"
-          content="ca-pub-8531177897035530"
-        />
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8531177897035530"
-          crossOrigin="anonymous"
-        ></script>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    const isAmp = this.props.__NEXT_DATA__?.isAmp;
+
+    return (
+      <Html lang="it">
+        <Head>
+          <meta
+            name="google-adsense-account"
+            content="ca-pub-8531177897035530"
+          />
+          {!isAmp && (
+            <script
+              async
+              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8531177897035530"
+              crossOrigin="anonymous"
+            ></script>
+          )}
+          {isAmp && (
+            <script
+              async
+              custom-element="amp-auto-ads"
+              src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js"
+            ></script>
+          )}
+        </Head>
+        <body>
+          {isAmp && (
+            <amp-auto-ads
+              type="adsense"
+              data-ad-client="ca-pub-8531177897035530"
+            ></amp-auto-ads>
+          )}
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
